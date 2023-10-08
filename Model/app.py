e3c851b6858099b79d61f8673f2367444d1c4374
Mohamed Hisham Abdelzaher <11410120200483@stud.cu.edu.eg>
2023-10-08 20:25:35 +0300

    Add files via upload

---
+++ Model/app.py
@@ -0,0 +1,19 @@
+import keras
+import gradio as gr
+
+model = keras.models.load_model("model.keras")
+
+
+def recognize_digit(img):
+    img = img.reshape(1, 784)
+    img = img / 255
+    prediction = model.predict(img).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
+
+
+gr.Interface(
+    recognize_digit,
+    inputs=gr.Sketchpad(),
+    outputs=gr.Label(),
+    title="Predict from 0 to 9",
+).launch()
